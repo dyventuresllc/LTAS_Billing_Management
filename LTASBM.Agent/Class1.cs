16265728c5a49f7fa56aa429fd636ee3ebac54c2
Damien Young <dyoung1006@gmail.com>
2024-10-26 09:26:40 -0700

    Add project files.

---
+++ LTASBM.Agent/Class1.cs
@@ -0,0 +1,31 @@
+﻿using kCura.Agent;
+using System;
+using System.Runtime.InteropServices;
+using LTASBM.Kepler.Interfaces.LTASBM.v1;
+using System.Collections.Generic;
+
+namespace LTASBM.Agent
+{
+    [kCura.Agent.CustomAttributes.Name("LTAS Billing Manangement")]
+    [Guid("fe5d6dca-598c-483f-942b-64af159f0982")]
+
+    public class LTASBillingWorker : AgentBase
+    {
+        public override string Name => "LTAS Billing Managment Worker";
+
+        public override void Execute()
+        {
+            using (var servicesManager = Helper.GetServicesManager())
+            {
+                string dB = "EDDS1623625"; 
+                string serverName = "esus02512841W05.sql-Y012.relativity.one\\esus02512841W05"; 
+
+                var keplerServiceProxy = servicesManager.CreateProxy<ILTASClient>();
+                List<LTASClient> clients = keplerServiceProxy.GetClients(dB, serverName).Result;
+
+
+            }
+
+        }
+    }
+}
---
+++ LTASBM.Kepler.Interfaces/LTASBM/v1/ILTASClient.cs
@@ -0,0 +1,22 @@
+﻿using Relativity.Kepler.Services;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LTASBM.Kepler.Interfaces.LTASBM.v1
+{
+    [ServiceModule("LTASBM Module")]
+    [RoutePrefix("LTASBM", VersioningStrategy.Namespace)]
+    public interface ILTASClient
+    {
+        [Route("GetClients")]
+        [HttpGet]
+        Task<List<LTASClient>> GetClients(string dB, string serverName);
+    }
+
+    public class LTASClient
+    {
+        public string Number { get; set; }
+        public string Name { get; set; }
+        public int CreatedBy { get; set; } 
+    }
+}
