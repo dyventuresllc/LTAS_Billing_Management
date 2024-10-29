c0228225a09ef29069ec1aaa7ea059f540661c4a
Damien Young <dyoung1006@gmail.com>
2024-10-29 14:26:51 -0700

    New methods for creating client/matters

--- LTASBM.Agent/Tasks/Tasks.cs
+++ LTASBM.Agent/Tasks/Tasks.cs
@@ -3,8 +3,12 @@ using LTASBM.Kepler.Interfaces.LTASBM.v1;
 using Relativity.API;
 using Relativity.Identity.V1.Services;
 using Relativity.Identity.V1.UserModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
+using Relativity.Services.Objects;
+using Relativity.Services.Objects.DataContracts;
 
 namespace LTASBM.Agent.Tasks
 {
@@ -23,6 +27,94 @@ namespace LTASBM.Agent.Tasks
                     UserResponse response = await userManager.ReadAsync(client.CreatedBy);
                     Emails.FixClientEmail(instanceSettingsBundle, response.EmailAddress, response.FirstName);
                 }
+            }
+        }
+
+        static async Task CreateNewClient(IObjectManager objectManager, int workspaceArtifactID, string clientIdValue, string clientNameValue)
+        {
+            Guid clientIdField = new Guid("C3F4236F-B59B-48B5-99C8-3678AA5EEA72");
+            Guid clientNameField = new Guid("C3F4236F-B59B-48B5-99C8-3678AA5EEA72");
+
+            try
+            {
+                var createRequest = new CreateRequest
+                {
+                    ObjectType = new ObjectTypeRef
+                    {
+                        Guid = new Guid("628EC03F-E789-40AF-AA13-351F92FFA44D")
+                    }
+                    ,
+                    FieldValues = new List<FieldRefValuePair>
+                    {
+                       new FieldRefValuePair
+                       {
+                           Field = new FieldRef
+                           {
+                            Guid = clientIdField
+                           },
+                           Value = clientIdValue
+                       },
+                       new FieldRefValuePair
+                       {
+                           Field = new FieldRef
+                           {
+                            Guid= clientNameField
+                           },
+                           Value = clientNameValue
+                       }
+                    }
+                };
+
+                using (objectManager)
+                {
+                    var result = await objectManager.CreateAsync(workspaceArtifactID, createRequest);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error creating client: {ex.Message}");
+            }
+        }
+
+        static async Task CreateNewMatter(IObjectManager objectManager, int workspaceId, string matterId, string matterName)
+        {
+            Guid matterIdField = new Guid("3A8B7AC8-0393-4C48-9F58-C60980AE8107");
+            Guid matterNameField = new Guid("C375AA14-D5CD-484C-91D5-35B21826AD14");
+            Guid MatterGuidField = new Guid("4E41FF7F-9D1C-4502-96D9-DFBB9252B3E6");
+
+            try
+            {
+                using (objectManager)
+                {
+                    var createRequest = new CreateRequest
+                    {
+                        ObjectType = new ObjectTypeRef { ArtifactTypeID = 1000116 },
+                        FieldValues = new List<FieldRefValuePair>
+                        {
+                            new FieldRefValuePair
+                            {
+                                Field = new FieldRef { Guid = matterIdField }, Value = matterId
+                            },
+                            new FieldRefValuePair
+                            {
+                                Field = new FieldRef { Guid = matterNameField }, Value= matterName
+                            },
+                            new FieldRefValuePair
+                            {
+                                Field = new FieldRef { Guid = MatterGuidField}, Value= Guid.NewGuid()
+                            }
+                        }
+                    };
+                    using (objectManager)
+                    {
+                        var result = await objectManager.CreateAsync(workspaceId, createRequest);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error creating matter: {ex.Message}");
+                Console.WriteLine($"Detail: {ex}");
             }
         }
     }
--- LTASBM.Kepler.Interfaces/LTASBM/ILTASBMSVCModule.cs
+++ LTASBM.Kepler.Interfaces/LTASBM/ILTASBMSVCModule.cs
@@ -11,5 +11,4 @@ namespace LTASBM.Kepler.Interfaces.LTASBM
     public interface ILTASBMSVCModule
     {        
     }
-
 }
